1e8e6943a0fae19761ac8154e3ca79cdd0422162
limgyumin <dev.limgyumin@gmail.com>
2022-01-23 00:53:43 +0900

    feat: app 컴포넌트 구성

---
+++ src/components/App.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const App: React.FC = () => {
+  return <div>This is App Component.</div>;
+};
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-ReactDOM.render(<div></div>, document.getElementById("root"));
+import App from "@/components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
