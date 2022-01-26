a72742d3e6975cec9c9ea94e41dcd8180223579f
limgyumin <dev.limgyumin@gmail.com>
2022-01-27 00:40:33 +0900

    refactor: 클래스별 역할이 명확하도록 MarkdownLoader에서 Parser를 사용하지 않도록 수정

--- src/libs/FileLoader/MarkdownLoader.ts
+++ src/libs/FileLoader/MarkdownLoader.ts
@@ -1,23 +1,6 @@
-import FilePathParser, { Item } from "../FilePathParser/FilePathParser";
 import FileLoader from "./FileLoader";
 
-class MarkdownLoader extends FileLoader {
-  findAllWithFormat(): Item[] {
-    const list: Item[] = [];
-
-    const parser = new FilePathParser();
-
-    this.paths.forEach((path) => {
-      const item = parser.parse(path);
-
-      if (item != null) {
-        list.push(item);
-      }
-    });
-
-    return list;
-  }
-}
+class MarkdownLoader extends FileLoader {}
 
 const postsContext = require.context("@/_posts", false, /\.md$/);
 
