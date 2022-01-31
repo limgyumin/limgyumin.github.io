05a7371abb57776f9eff32584f83850570e2a08d
limgyumin <whswkfrbals4348@gmail.com>
2022-01-31 15:51:28 +0900

    chore: styled-components 환경 설정

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,19 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
 
 import App from "@/components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import { theme } from "./styles/theme";
+import { GlobalStyles } from "./styles/globalStyles";
+
+ReactDOM.render(
+  <BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <App />
+    </ThemeProvider>
+  </BrowserRouter>,
+  document.getElementById("root"),
+);
