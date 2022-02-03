c5433bd576d59144761a0c32dcf19cedf35a5bf5
limgyumin <dev.limgyumin@gmail.com>
2022-02-03 16:46:47 +0900

    refactor: MetaDataParser에서 데이터 검증을 하지 않도록 수정

--- src/libs/Parser/MetaDataParser.ts
+++ src/libs/Parser/MetaDataParser.ts
@@ -1,29 +1,17 @@
-import { PostInitializer } from "@/models/post";
-
-const initialized: PostInitializer = {
-  title: "",
-  description: "",
-  category: "",
-  createdAt: "",
-};
-
+type MetaData = { [key: string]: string | null };
 class MetaDataParser {
-  toObject(content: string): PostInitializer {
+  toObject(content: string): MetaData {
     const regExp = /(\w+):\s?(.+)/gm;
     const matchedList = [...content.matchAll(regExp)];
 
-    const object: PostInitializer = matchedList.reduce((acc, matched) => {
+    const object: MetaData = matchedList.reduce((acc, matched) => {
       const [, key, value] = matched;
-      const label = key as keyof PostInitializer;
-
-      if (!Object.prototype.hasOwnProperty.call(initialized, label)) {
-        throw new Error(`${key} is not a property of meta.`);
-      }
+      const label = key as keyof MetaData;
 
       acc[label] = value;
 
       return acc;
-    }, initialized);
+    }, {} as MetaData);
 
     return object;
   }
