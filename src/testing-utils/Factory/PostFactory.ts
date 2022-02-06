a9940e47aeac23f55d8536f4ed32d4be40a07db7
limgyumin <whswkfrbals4348@gmail.com>
2022-02-07 00:25:57 +0900

    feat: 제네릭 타입에 해당하는 객체 또는 객체 배열을 생성해주는 Factory 클래스 생성

---
+++ src/testing-utils/Factory/PostFactory.ts
@@ -0,0 +1,15 @@
+import faker from "faker";
+
+import Factory from "./Factory";
+
+import Post from "@/models/post";
+
+const postFactory = new Factory<Post>({
+  id: () => faker.random.alphaNumeric(7),
+  title: () => faker.name.title(),
+  description: () => faker.lorem.sentence(),
+  category: () => faker.lorem.word(),
+  createdAt: () => faker.date.past().toISOString(),
+});
+
+export default postFactory;
