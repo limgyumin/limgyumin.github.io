eaa3218142895522229123a5cc919df686617247
limgyumin <whswkfrbals4348@gmail.com>
2022-02-07 00:40:31 +0900

    feat: 글 목록 조회 및 페이지 이동이 가능한 페이지 컴포넌트 구성

---
+++ src/components/PostList/PostItem/index.spec.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+import PostItem from ".";
+import { render, screen } from "@/testing-utils";
+import postFactory from "@/testing-utils/Factory/PostFactory";
+
+describe("PostItem", () => {
+  describe("게시글 아이템이 렌더되면", () => {
+    beforeEach(() => {
+      const post = postFactory.build();
+
+      render(<PostItem post={post} />, {});
+    });
+
+    it("제목을 확인할 수 있어야 한다.", () => {
+      expect(screen.getByTestId("post-category")).toBeInTheDocument();
+    });
+
+    it("설명을 확인할 수 있어야 한다.", () => {
+      expect(screen.getByTestId("post-description")).toBeInTheDocument();
+    });
+
+    it("카테고리를 확인할 수 있어야 한다.", () => {
+      expect(screen.getByTestId("post-category")).toBeInTheDocument();
+    });
+
+    it("작성일을 확인할 수 있어야 한다.", () => {
+      expect(screen.getByTestId("post-date")).toBeInTheDocument();
+    });
+  });
+});
