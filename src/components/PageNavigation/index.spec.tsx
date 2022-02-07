984f05589677a3750b908151f478b06fac9367da
limgyumin <whswkfrbals4348@gmail.com>
2022-02-07 23:41:07 +0900

    feat: PageNavigation unit test 추가

---
+++ src/components/PageNavigation/index.spec.tsx
@@ -0,0 +1,88 @@
+import React from "react";
+
+import PageNavigation from ".";
+import { render, screen, fireEvent, act } from "@/testing-utils";
+import postFactory from "@/testing-utils/Factory/PostFactory";
+import postStore from "@/stores/postStore";
+import { POST_COUNT_PER_FETCH } from "@/constants/post";
+
+const mockPosts = postFactory.buildList(5 * POST_COUNT_PER_FETCH);
+
+jest.mock("@/repositories/postRepository", () =>
+  jest.fn().mockImplementation(() => ({
+    fetchPosts() {
+      return mockPosts;
+    },
+  })),
+);
+
+describe("PageNavigation", () => {
+  describe("컴포넌트가 렌더되면", () => {
+    beforeEach(() => {
+      render(<PageNavigation />, {});
+
+      postStore.fetchPosts();
+    });
+
+    it("페이지 네비게이션을 확인할 수 있다.", () => {
+      expect(screen.getByTestId("page-navigation")).toBeInTheDocument();
+    });
+  });
+
+  describe("글 목록의 이전 페이지가 존재하면", () => {
+    beforeEach(async () => {
+      render(<PageNavigation />, {});
+
+      await postStore.fetchPosts();
+      postStore.setPage(2);
+    });
+
+    describe("이전 페이지 버튼을 클릭했을 때", () => {
+      it("페이지가 감소해야 한다.", () => {
+        act(() => {
+          fireEvent.click(screen.getByTestId("page-navigation-prev-btn"));
+        });
+
+        expect(postStore.page).toBe(1);
+      });
+    });
+  });
+
+  describe("글 목록의 다음 페이지가 존재하면", () => {
+    beforeEach(async () => {
+      render(<PageNavigation />, {});
+
+      await postStore.fetchPosts();
+      postStore.setPage(2);
+    });
+
+    describe("다음 페이지 버튼을 클릭했을 때", () => {
+      it("페이지가 증가해야 한다.", () => {
+        act(() => {
+          fireEvent.click(screen.getByTestId("page-navigation-next-btn"));
+        });
+
+        expect(postStore.page).toBe(3);
+      });
+    });
+  });
+
+  describe("글 목록의 이전 페이지가 존재하지 않으면", () => {
+    beforeEach(async () => {
+      render(<PageNavigation />, {});
+
+      await postStore.fetchPosts();
+      postStore.setPage(0);
+    });
+
+    describe("이전 페이지 버튼을 클릭했을 때", () => {
+      it("페이지가 감소하지 않아야 한다.", () => {
+        act(() => {
+          fireEvent.click(screen.getByTestId("page-navigation-prev-btn"));
+        });
+
+        expect(postStore.page).toBe(0);
+      });
+    });
+  });
+});
