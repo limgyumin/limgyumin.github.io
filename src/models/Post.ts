60213f977c2af37402748e9f8f91075ba0c75225
limgyumin <whswkfrbals4348@gmail.com>
2022-02-13 01:16:30 +0900

    refactor: Post model의 instance를 생성하는 static 함수를 통해 instance를 생성하도록 리팩토링

--- src/models/Post.ts
+++ src/models/Post.ts
@@ -1,3 +1,5 @@
+import postValidator from "@/libs/Validator/PostValidator";
+
 export type PostInitializer = {
   id: string;
   title: string;
@@ -19,6 +21,14 @@ class Post {
     this.description = post.description;
     this.category = post.category;
     this.createdAt = post.createdAt;
+  }
+
+  static makeInstance(post: PostInitializer): Post {
+    const instance = new Post(post);
+
+    postValidator.validate(instance);
+
+    return instance;
   }
 }
 
