fff298047502c3c6839441c33f221fb7daa8ec24
limgyumin <dev.limgyumin@gmail.com>
2022-03-09 21:34:05 +0900

    refactor: postRepository의 메서드의 return type과 일관되도록 PostFactory의 generic type 수정

--- src/testing-utils/Factory/PostFactory.ts
+++ src/testing-utils/Factory/PostFactory.ts
@@ -2,9 +2,9 @@ import faker from "faker";
 
 import Factory from "./Factory";
 
-import Post from "@/models/Post";
+import { PostInitializer } from "@/models/Post";
 
-const postFactory = new Factory<Post>({
+const postFactory = new Factory<PostInitializer>({
   id: () => faker.random.alphaNumeric(7),
   title: () => faker.name.title(),
   description: () => faker.lorem.sentence(),
