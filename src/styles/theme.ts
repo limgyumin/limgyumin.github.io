d833f3294dbbec9813e40d59d3a8da9b9e8d7c6a
limgyumin <dev.limgyumin@gmail.com>
2022-03-11 01:20:02 +0900

    chore: color variable 추가, colors와 medias를 개별 파일로 분리

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,34 +1,5 @@
-const colors = {
-  bgWhite: "#fff",
-  bgGray: "#f0f0f0",
-  bgBlack: "#1b1b1b",
-
-  ftWhite: "#f8f8f8",
-  ftBlack: "#1b1b1b",
-  ftGray: "#d9d9d9",
-
-  boxShadow: "rgba(0,0,0,0.4)",
-};
-
-const sizes = {
-  mobile: 580,
-  tablet: 768,
-  smallDesktop: 1044,
-  desktop: 1284,
-};
-
-type SizeLabels = keyof typeof sizes;
-type Media = { [key in SizeLabels]: string };
-
-const sizeLabels = Object.keys(sizes) as SizeLabels[];
-
-const mediaQuery = (value: number) =>
-  `@media only screen and (max-width: ${value}px)`;
-
-const medias: Media = sizeLabels.reduce((acc, label) => {
-  acc[label] = mediaQuery(sizes[label]);
-  return acc;
-}, {} as Media);
+import { colors } from "./colors";
+import { medias } from "./medias";
 
 export const theme = {
   colors,
