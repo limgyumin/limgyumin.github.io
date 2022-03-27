cd90c0983421baf250653e706ad4e69a96bf553c
limgyumin <dev.limgyumin@gmail.com>
2022-03-27 22:55:46 +0900

    refactor: 배열을 생성자로 받는 경우 배열 값을 복사하도록 수정

--- src/utils/list.ts
+++ src/utils/list.ts
@@ -3,44 +3,47 @@ import Sort, { OrderMethod } from "@/utils/sort";
 
 class List<T extends object> {
   private total: number = 0;
+  private _list: T[] = [];
 
-  constructor(private list: T[] = []) {}
+  constructor(list: T[]) {
+    this._list = [...list];
+  }
 
   where(key: keyof T, fn: (value: T[keyof T]) => boolean): this {
-    const filtered = this.list.filter((obj) => fn(obj[key]));
+    const filtered = this._list.filter((obj) => fn(obj[key]));
 
-    this.list = filtered;
+    this._list = filtered;
 
     return this;
   }
 
   orderBy(key: keyof T, method: OrderMethod = "ASC"): this {
-    const sorted = new Sort(this.list).orderBy(key, method);
+    const sorted = new Sort(this._list).orderBy(key, method);
 
-    this.list = sorted;
+    this._list = sorted;
 
     return this;
   }
 
   paginate(offset: number, limit: number): this {
-    const { list, total } = new Page(this.list).skip(offset).take(limit);
+    const { list, total } = new Page(this._list).skip(offset).take(limit);
 
-    this.list = list;
+    this._list = list;
     this.total = total;
 
     return this;
   }
 
   getOne(): T {
-    const [item] = this.list;
+    const [item] = this._list;
 
     return item;
   }
 
   getMany(): { list: T[]; total: number } {
-    const total = this.total || this.list.length;
+    const total = this.total || this._list.length;
 
-    return { list: this.list, total };
+    return { list: this._list, total };
   }
 }
 
