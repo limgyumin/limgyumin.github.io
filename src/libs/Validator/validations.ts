67d85bdbc5f536d1084a2ecae32a55d2d3f35b1d
limgyumin <dev.limgyumin@gmail.com>
2022-04-11 18:29:42 +0900

    feat: 정해진 날짜 양식을 지키도록 validation 함수 추가 및 적용

--- src/libs/Validator/validations.ts
+++ src/libs/Validator/validations.ts
@@ -1,17 +1,19 @@
 import { State, Validation } from "./Validator";
 
-export const isRequired: Validation<string> = (
-  value: string | null | undefined,
-) => {
-  if (value != null) {
-    return { state: State.Success };
-  }
-  return { state: State.Failure, reason: "Value is not required." };
-};
-
-export const isNotEmpty: Validation<string> = (value: string) => {
+export const isNotEmpty: Validation<string> = (value) => {
   if (value.replace(/\s/gm, "") !== "") {
     return { state: State.Success };
   }
+
   return { state: State.Failure, reason: "Value is empty." };
+};
+
+export const isDate: Validation<string> = (value) => {
+  const regex = /\d{4}-\d{1,2}-\d{1,2}/g;
+
+  if (value.match(regex)) {
+    return { state: State.Success };
+  }
+
+  return { state: State.Failure, reason: "Value is not a date." };
 };
