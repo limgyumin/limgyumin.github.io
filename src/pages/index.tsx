ed18f214511c7c85defe664569b482f1b56bb6ed
limgyumin <dev.limgyumin@gmail.com>
2022-04-19 00:08:32 +0900

    feat: loadable components를 사용하여 Page 단위로 code spliting 적용

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,18 @@
-export { default as Main } from "./MainPage";
-export { default as PostDetail } from "./PostDetailPage";
-export { default as Search } from "./SearchPage";
+import React from "react";
+import loadable from "@loadable/component";
+
+import LoadingIndicator from "@/components/common/molecules/LoadingIndicator";
+
+const Main = loadable(() => import("./MainPage"), {
+  fallback: <LoadingIndicator />,
+});
+
+const PostDetail = loadable(() => import("./PostDetailPage"), {
+  fallback: <LoadingIndicator />,
+});
+
+const Search = loadable(() => import("./SearchPage"), {
+  fallback: <LoadingIndicator />,
+});
+
+export { Main, PostDetail, Search };
